e0f5c1a96261a92583a12e1bb16fb6566b17ced6
Dariusz Markiewicz <97259830+dariuszmarkiewicz@users.noreply.github.com>
2022-01-07 13:33:10 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    // int a = 10, b = 20;
+    // cout << a << " - " << b << endl;
+    // swap(a, b);
+    // cout << a << " - " << b << endl;
+
+    cout << endl;
+
+    char z_1 = 'A', z_2 = 'B';
+    cout << z_1 << " - " << z_2 << endl;
+    swap(z_1, z_2);
+    cout << z_1 << " - " << z_2 << endl;
+
+    cout << endl;
+
+    string miasto_1 = "OLKUSZ", miasto_2 = "WOLBROM";
+    cout << miasto_1 << " - " << miasto_2 << endl;
+    swap(miasto_1, miasto_2);
+    cout << miasto_1 << " - " << miasto_2 << endl;
+
+    cout << endl;
+
+    return 0;
+}
